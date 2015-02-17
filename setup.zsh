da7e28f3eb8662660226ada1b829d7de6de1c1d5
James Ye <lolz836@gmail.com>
2015-02-17 19:07:26 +1100

    Add setup script

---
+++ setup.zsh
@@ -0,0 +1,10 @@
+#!/usr/bin/zsh
+git submodule init
+git submodule update
+ln -s $PWD/.oh-my-zsh $HOME/.oh-my-zsh
+ln -s $PWD/.zsh-syntax-highlighting .oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+ln -s $PWD/.zshrc $HOME/.zshrc
+ln -s $PWD/.vimrc $HOME/.vimrc
+ln -s $PWD/.vim $HOME/.vim
+ln -s $PWD/.tmux.conf $HOME/.tmux.conf
+ln -s $PWD/.tmuxline.conf $HOME/.tmuxline.conf
