a1671b80cf8192e3f10746f4ecb8d0fc199a3d05
James Ye <lolz836@gmail.com>
2015-02-23 19:24:05 +1100

    .vimrc in .vim/

--- setup.zsh
+++ setup.zsh
@@ -3,7 +3,6 @@ git submodule update --init --recursive --remote
 ln -s $PWD/.oh-my-zsh $HOME/.oh-my-zsh
 ln -s $PWD/.zsh-syntax-highlighting .oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 ln -s $PWD/.zshrc $HOME/.zshrc
-ln -s $PWD/.vimrc $HOME/.vimrc
 ln -s $PWD/.vim $HOME/.vim
 ln -s $PWD/.tmux.conf $HOME/.tmux.conf
 ln -s $PWD/.tmuxline.conf $HOME/.tmuxline.conf
