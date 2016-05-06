975ca66f5f1bcacf790489c9cd0a4df145159ab7
James Ye <lolz836@gmail.com>
2016-05-06 20:59:49 +1000

    updoot

--- setup.sh
+++ setup.sh
@@ -1,28 +1,18 @@
-#!/usr/bin/sh
+#!/usr/bin/env zsh
 printf "Updating submodules. Please wait..."
 git submodule update --init --recursive --remote
 printf "done\n"
 
-vared -p "Install Xresources? yes/[no] " -c xresources
-vared -p "Install i3 config? yes/[no] " -c i3
-vared -p "Install extra X application config? yes/[no] " -c extra
-vared -p "Install termite config? yes/[no] " -c termite
+vared -p "redshift config? yes/[no] " -c redshift
 
 if [[ ! -a $HOME/.config ]]; then
 	mkdir $HOME/.config
 fi
 
-if command -v zsh > /dev/null 2>&1; then
-	ln -s $PWD/.oh-my-zsh $HOME/.oh-my-zsh
-	ln -s $PWD/zsh-syntax-highlighting $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-	ln -s $PWD/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-	ln -s $PWD/.zshrc $HOME/.zshrc
-	cp $PWD/.zshrc.local $HOME/.zshrc.local
-	echo "Installed zsh config"
-else
-	cat $PWD/.bashrc >> ~/.bashrc
-	echo "Installed bash config"
-fi
+ln -s $PWD/.zprezto $HOME/.zprezto
+cp $PWD/.zpreztorc $HOME/.zprezto/runcoms/zpreztorc
+cp $PWD/.zshrc $HOME/.zshrc
+echo "Installed zsh config"
 
 ln -s $PWD/.vim $HOME/.vim
 echo "Installed vim config"
@@ -33,26 +23,8 @@ if command -v tmux > /dev/null 2>&1; then
 	echo "Installed tmux config"
 fi
 
-if [[ $xresources == "yes" ]]; then
-	ln -s $PWD/.Xresources $HOME/.Xresources
-	echo "Installed Xresources"
-fi
-
-if command -v i3 > /dev/null 2>&1; then
-	ln -s $PWD/.i3 $HOME/.i3
-	ln -s $PWD/.i3status.conf $HOME/.i3status.conf
-	echo "Installed i3 config"
-fi
-
-
-if [[ $extra == "yes" ]]; then
-	ln -s $PWD/.config/compton.conf $HOME/.config/compton.conf
-	ln -s $PWD/.config/dunst $HOME/.config/dunst
-	echo "Installed extra X applications config"
-fi
-
-if command -v termite > /dev/null 2>&1; then
-	ln -s $PWD/.config/termite $HOME/.config/termite
-	echo "Installed termite config"
+if [[ $redshift == "yes" ]]; then
+	ln -s $PWD/.config/redshift.conf $HOME/.config/redshift.conf
+	echo "Installed redshift config"
 fi
 
