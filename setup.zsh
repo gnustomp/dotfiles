85ec600ab97d72a784065441549858b4c2ad5a00
James Ye <lolz836@gmail.com>
2017-01-31 19:33:33 +0800

    update

--- setup.zsh
+++ setup.zsh
@@ -4,6 +4,7 @@ git submodule update --init --recursive --remote
 printf "done\n"
 
 vared -p "redshift config? yes/[no] " -c redshift
+vared -p "install YouCompleteMe? yes/[no] " -c ycm
 
 if [[ ! -a $HOME/.config ]]; then
 	mkdir $HOME/.config
@@ -28,10 +29,15 @@ if command -v tmux > /dev/null 2>&1; then
 	ln -s $PWD/.tmux.conf $HOME/.tmux.conf
 	ln -s $PWD/.tmuxline.conf $HOME/.tmuxline.conf
 	echo "Installed tmux config"
+else
+	echo "Did not install tmux config"
 fi
 
 if [[ $redshift == "yes" ]]; then
 	ln -s $PWD/.config/redshift.conf $HOME/.config/redshift.conf
 	echo "Installed redshift config"
+else
+	echo "Did not install redshift config"
 fi
 
+if [[ $ycm == "yes" ]]; then
